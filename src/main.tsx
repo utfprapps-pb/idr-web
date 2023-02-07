42a1bad761aa5f4f5291da75c73efffa84950c67
Guilherme Minozzi <minozzi60@gmail.com>
2023-02-07 14:30:41 -0300

    chore: add project configs (#1)
    * chore: add editorconfig
    
    * chore: add commitlint
    
    * chore: add eslint
    
    * chore: add husky, prettier and commitlint config conventional
    
    * feat: add router
    
    * chore: add dynamic path

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import { Router } from "@/main/routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<BrowserRouter>
+			<Router />
+		</BrowserRouter>
+	</React.StrictMode>
+);
---
+++ src/main/routes/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+
+import { ROUTES } from "./routes";
+
+export const Router: React.FC = () => {
+	return (
+		<Routes>
+			{Object.entries(ROUTES).map(([key, route]) => (
+				<Route key={key} path={route.path()} element={route.component} />
+			))}
+
+			<Route path="*" element={<h1>404</h1>} />
+		</Routes>
+	);
+};
