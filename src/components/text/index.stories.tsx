6193a1dba41a1a2ad45659cec5e94a72a1466d0f
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-06 21:39:02 -0300

    feat: add text component

---
+++ src/components/text/index.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+
+import { Text, TextProps } from '.'
+
+export default {
+	title: 'Components/Text',
+	component: Text
+} as Meta
+
+const Template: StoryFn<TextProps> = (args) => <Text {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	children: 'Text Example',
+	size: 'b1'
+}
---
+++ src/components/text/types.ts
@@ -0,0 +1,63 @@
+import { PropsWithChildren } from 'react'
+
+import { Theme } from '@/styles'
+
+type Props = {
+	/**
+	 * Size of the text
+	 *
+	 * @example
+	 * ```tsx
+	 * <Text size="b1" />
+	 * ```
+	 */
+	size: keyof Theme['fontSizes']
+
+	/**
+	 * Color of the text
+	 *
+	 * @example
+	 * ```tsx
+	 * <Text color="primary" />
+	 * ```
+	 */
+	color?: keyof Theme['colors']
+
+	/**
+	 * The family of the font
+	 *
+	 * @example
+	 * ```tsx
+	 * <Text fontFamily="secondary" />
+	 * ```
+	 */
+	fontFamily?: keyof Theme['fontFamily']
+
+	/**
+	 * The class name of the text
+	 *
+	 * @example
+	 * ```tsx
+	 * <Text className="my-class" />
+	 * ```
+	 */
+	className?: string
+
+	/**
+	 * Function to be called when the text is clicked
+	 *
+	 * @example
+	 * ```tsx
+	 * <Text onClick={() => console.log('Text clicked')} />
+	 * ```
+	 */
+	onClick?: () => void
+}
+
+export type TextProps = PropsWithChildren<Props>
+
+export type DesignSystemMapperProps = {
+	[key in keyof Theme['fontSizes']]: {
+		weight: keyof Theme['fontWeights']
+	}
+}
