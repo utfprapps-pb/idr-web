402bf96ef29f3e31d643cc5c277ba4d9afe586a6
Minozzzi <Minozzzi@users.noreply.github.com>
2023-03-15 00:21:39 +0000

    Deploying to main from  @ 29211d0ffe5ae538e89b7e6043d42542af6afc43 🚀

---
+++ docs/assets/index.stories.dacd281c.js
@@ -0,0 +1,20 @@
+var r=Object.defineProperty;var e=(t,n)=>r(t,"name",{value:n,configurable:!0});import{a as o}from"./index.1d24d3bd.js";import{j as a}from"./jsx-runtime.ba182e87.js";import"./styled-components.browser.esm.c296be4a.js";import"./index.c6dec4e6.js";import"./reset.5149fadc.js";import"./iframe.3819cdfe.js";const f={parameters:{storySource:{source:`import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+
+import { Text, TextProps } from '.'
+
+export default {
+	title: 'Components/Text',
+	component: Text
+} as Meta
+
+const Template: StoryFn<TextProps> = (args) => <Text {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	children: 'Text Example',
+	size: 'b1'
+}
+`,locationsMap:{default:{startLoc:{col:37,line:12},endLoc:{col:65,line:12},startBody:{col:37,line:12},endBody:{col:65,line:12}}}}},title:"Components/Text",component:o},s=e(t=>a(o,{...t}),"Template"),p=s.bind({});p.args={children:"Text Example",size:"b1"};const u=["Default"];export{p as Default,u as __namedExportsOrder,f as default};
+//# sourceMappingURL=index.stories.dacd281c.js.map
---
+++ docs/assets/reset.5149fadc.js
@@ -0,0 +1,17 @@
+var n=Object.defineProperty;var r=(e,t)=>n(e,"name",{value:t,configurable:!0});import{W as l}from"./styled-components.browser.esm.c296be4a.js";const i=Object.freeze({xsmall:600,small:960,medium:1280,large:1920}),a=r(e=>`${e}px`,"extractBreakpointValue"),m={colors:{primary:"#0B71B9",secondary:"#038657",white:"#FFFFFF",black:"#070813",lightgray:"#8D8D8D",gray:"#444545",darkgray:"#202328",background:"#F7F7F7",error:"#D51A52"},fontFamily:{primary:"'Figtree', 'Roboto'"},fontSizes:{h1:"104px",h2:"76px",h3:"48px",h4:"32px",h5:"24px",b1:"24px",b2:"20px",b3:"16px",b4:"12px"},fontWeights:{light:"400",regular:"500",bold:"700"},media:{lessThan:e=>`@media(max-width: ${a(e)})`,between:(e,t)=>{const o=a(e),d=a(t);return`@media (min-width: ${o}) and (max-width: ${d})`},greaterThan:e=>`@media(min-width: ${a(e)})`,forPhoneOnly:()=>`@media(max-width: ${a(i.xsmall)})`,forTablePortraitUp:()=>`@media(min-width: ${a(i.xsmall)})`,forTablePortraitOnly:()=>`@media(min-width: ${a(i.xsmall)}) and (max-width: ${a(i.small)})`,forTableLandscapeUp:()=>`@media(min-width: ${a(i.small)})`,forTableLandscapeOnly:()=>`@media(min-width: ${a(i.small)}) and (max-width: ${a(i.medium)})`,forDesktopUp:()=>`@media(min-width: ${a(i.medium)})`,forDesktopOnly:()=>`@media(min-width: ${a(i.medium)}) and (max-width: ${a(i.large)})`,forBigDesktopUp:()=>`@media(min-width: ${a(i.large)})`}},h=l`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  button, link, input[type="submit"] {
+    cursor: pointer;
+  }
+
+  body {
+		font-family: ${m.fontFamily.primary};
+		background-color: ${m.colors.background};
+  }
+`;export{h as R,m as t};
+//# sourceMappingURL=reset.5149fadc.js.map
