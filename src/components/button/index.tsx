b386ff1a171c171a387658beac1b3cef41c2ffb3
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-19 18:39:12 -0300

    feat(components): add button

---
+++ src/components/button/index.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo } from 'react'
+
+import * as S from './styles'
+import { buttonThemes, ButtonThemes } from './theme'
+import { ButtonProps } from './types'
+
+export * from './types'
+export * from './theme'
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+	(
+		{
+			children,
+			disabled = false,
+			theme = ButtonThemes.primary,
+			type = 'button',
+			icon,
+			onClick = () => null,
+			...props
+		},
+		ref
+	) => {
+		const buttonTheme = useMemo(() => buttonThemes[theme], [theme])
+
+		return (
+			<S.Container>
+				<S.Button
+					buttonTheme={buttonTheme}
+					ref={ref}
+					type={type}
+					disabled={disabled}
+					onClick={onClick}
+					{...props}
+				>
+					{icon}
+					{children}
+				</S.Button>
+			</S.Container>
+		)
+	}
+)
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export * from '@/components/button'
 export * from '@/components/inputs'
 export * from '@/components/text'
 export * from '@/components/typography'
