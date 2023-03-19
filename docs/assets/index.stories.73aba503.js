0170beb4f0db84b4412012bf0fdeceed11cbda38
Minozzzi <Minozzzi@users.noreply.github.com>
2023-03-19 21:42:24 +0000

    Deploying to main from  @ e7c0f0c140670f9c04ee475215361cb94e420ae8 🚀

---
+++ docs/assets/index.stories.73aba503.js
@@ -0,0 +1,20 @@
+var r=Object.defineProperty;var e=(t,n)=>r(t,"name",{value:n,configurable:!0});import{a as o}from"./index.9f635fb4.js";import{j as a}from"./jsx-runtime.d78ac174.js";import"./index.939cebbc.js";import"./styled-components.browser.esm.2d3d5857.js";import"./reset.45b08a7a.js";import"./index.27cabe32.js";import"./iframe.35550fff.js";const u={parameters:{storySource:{source:`import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+
+import { Text, TextProps } from '.'
+
+export default {
+	title: 'Components/Text',
+	component: Text
+} as Meta
+
+const Template: StoryFn<TextProps> = (args) => <Text {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	children: 'Text Example',
+	size: 'b1'
+}
+`,locationsMap:{default:{startLoc:{col:37,line:12},endLoc:{col:65,line:12},startBody:{col:37,line:12},endBody:{col:65,line:12}}}}},title:"Components/Text",component:o},s=e(t=>a(o,{...t}),"Template"),p=s.bind({});p.args={children:"Text Example",size:"b1"};const y=["Default"];export{p as Default,y as __namedExportsOrder,u as default};
+//# sourceMappingURL=index.stories.73aba503.js.map
---
+++ docs/assets/index.stories.8a566b42.js
@@ -0,0 +1,21 @@
+var a=Object.defineProperty;var o=(t,r)=>a(t,"name",{value:r,configurable:!0});import{B as n}from"./index.939cebbc.js";import{j as e}from"./jsx-runtime.d78ac174.js";import{L as s}from"./Lock.esm.e72630d4.js";import"./styled-components.browser.esm.2d3d5857.js";import"./reset.45b08a7a.js";import"./iframe.35550fff.js";const x={parameters:{storySource:{source:`import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+import { Lock } from 'phosphor-react'
+
+import { Button, ButtonProps } from '.'
+
+export default {
+	title: 'Components/Button',
+	component: Button
+} as Meta
+
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	children: 'Label Example',
+	icon: <Lock />
+}
+`,locationsMap:{default:{startLoc:{col:39,line:13},endLoc:{col:69,line:13},startBody:{col:39,line:13},endBody:{col:69,line:13}}}}},title:"Components/Button",component:n},p=o(t=>e(n,{...t}),"Template"),c=p.bind({});c.args={children:"Label Example",icon:e(s,{})};const L=["Default"];export{c as Default,L as __namedExportsOrder,x as default};
+//# sourceMappingURL=index.stories.8a566b42.js.map
