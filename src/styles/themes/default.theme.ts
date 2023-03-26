dc4f1a5457fc48dbbdfe0f9775565ff92b120b43
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-25 23:34:23 -0300

    style: change black color and font family

--- src/styles/themes/default.theme.ts
+++ src/styles/themes/default.theme.ts
@@ -14,7 +14,7 @@ export const theme: Theme = {
 		primary: '#0B71B9',
 		secondary: '#038657',
 		white: '#FFFFFF',
-		black: '#070813',
+		black: '#424242',
 		lightgray: '#8D8D8D',
 		gray: '#444545',
 		darkgray: '#202328',
@@ -22,7 +22,8 @@ export const theme: Theme = {
 		error: '#D51A52'
 	},
 	fontFamily: {
-		primary: "'Figtree', 'Roboto'"
+		primary:
+			"'Work Sans', '-apple-system', 'Roboto', 'Helvetica', 'Arial', sans-serif"
 	},
 	fontSizes: {
 		h1: '104px',
@@ -43,34 +44,44 @@ export const theme: Theme = {
 	media: {
 		lessThan: (breakpoint: number): string =>
 			`@media(max-width: ${extractBreakpointValue(breakpoint)})`,
+
 		between: (first: number, second: number): string => {
 			const parsedFirst = extractBreakpointValue(first)
 			const parsedSecond = extractBreakpointValue(second)
 
 			return `@media (min-width: ${parsedFirst}) and (max-width: ${parsedSecond})`
 		},
+
 		greaterThan: (breakpoint: number): string =>
 			`@media(min-width: ${extractBreakpointValue(breakpoint)})`,
+
 		forPhoneOnly: (): string =>
 			`@media(max-width: ${extractBreakpointValue(breakpoints.xsmall)})`,
+
 		forTablePortraitUp: (): string =>
 			`@media(min-width: ${extractBreakpointValue(breakpoints.xsmall)})`,
+
 		forTablePortraitOnly: (): string =>
 			`@media(min-width: ${extractBreakpointValue(
 				breakpoints.xsmall
 			)}) and (max-width: ${extractBreakpointValue(breakpoints.small)})`,
+
 		forTableLandscapeUp: (): string =>
 			`@media(min-width: ${extractBreakpointValue(breakpoints.small)})`,
+
 		forTableLandscapeOnly: (): string =>
 			`@media(min-width: ${extractBreakpointValue(
 				breakpoints.small
 			)}) and (max-width: ${extractBreakpointValue(breakpoints.medium)})`,
+
 		forDesktopUp: (): string =>
 			`@media(min-width: ${extractBreakpointValue(breakpoints.medium)})`,
+
 		forDesktopOnly: (): string =>
 			`@media(min-width: ${extractBreakpointValue(
 				breakpoints.medium
 			)}) and (max-width: ${extractBreakpointValue(breakpoints.large)})`,
+
 		forBigDesktopUp: (): string =>
 			`@media(min-width: ${extractBreakpointValue(breakpoints.large)})`
 	}
