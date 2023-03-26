1ec48f274c9bcb298c08987573ef6f0135276a53
Minozzzi <Minozzzi@users.noreply.github.com>
2023-03-26 17:59:47 +0000

    Deploying to main from  @ 438c8cafb5e8fbafd58f108ce33d3e5edf3c71d4 🚀

---
+++ docs/assets/index.stories.ed7aaee0.js
@@ -0,0 +1,21 @@
+var a=Object.defineProperty;var o=(t,r)=>a(t,"name",{value:r,configurable:!0});import{B as n}from"./index.24105264.js";import{j as e}from"./jsx-runtime.6092e680.js";import{L as s}from"./Lock.esm.e7f61654.js";import"./styled-components.browser.esm.94ce6fb2.js";import"./reset.aa460e6b.js";import"./iframe.54871c50.js";const x={parameters:{storySource:{source:`import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+import { Lock } from 'phosphor-react'
+
+import { Button, ButtonProps } from '.'
+
+export default {
+	title: 'Components/Button',
+	component: Button
+} as Meta
+
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+	children: 'Label Example',
+	icon: <Lock />
+}
+`,locationsMap:{default:{startLoc:{col:39,line:13},endLoc:{col:69,line:13},startBody:{col:39,line:13},endBody:{col:69,line:13}}}}},title:"Components/Button",component:n},p=o(t=>e(n,{...t}),"Template"),c=p.bind({});c.args={children:"Label Example",icon:e(s,{})};const L=["Default"];export{c as Default,L as __namedExportsOrder,x as default};
+//# sourceMappingURL=index.stories.ed7aaee0.js.map
