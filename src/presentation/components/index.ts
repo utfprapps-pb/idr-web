3138258034e7adf21b8c4d0c5584aed7c3d00e23
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-25 23:27:35 -0300

    feat(presentation/components): add loading component

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,4 +1,5 @@
 export * from '@/presentation/components/button'
 export * from '@/presentation/components/inputs'
+export * from '@/presentation/components/loading'
 export * from '@/presentation/components/text'
 export * from '@/presentation/components/typography'
---
+++ src/presentation/components/loading/styles.ts
@@ -0,0 +1,22 @@
+import styled, { keyframes } from 'styled-components'
+
+import { LoadingProps } from './types'
+
+const spin = keyframes`
+	to {
+		transform: rotate(360deg);
+	}
+`
+
+export const Loading = styled.section<LoadingProps>`
+	::before {
+		content: '';
+		display: block;
+		width: ${({ size }) => size}px;
+		height: ${({ size }) => size}px;
+		animation: ${spin} 0.5s infinite linear;
+		border: 5px ${({ theme }) => theme.colors.primary} solid;
+		border-left-color: ${({ theme }) => theme.colors.white};
+		border-radius: 100%;
+	}
+`
