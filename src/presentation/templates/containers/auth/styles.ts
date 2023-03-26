5eba1287c512a5201b53ae966f802d1582b533d6
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-25 23:33:28 -0300

    feat(presentation/templates): add auth container

---
+++ src/presentation/templates/containers/auth/styles.ts
@@ -0,0 +1,79 @@
+import styled, { CSSProperties } from 'styled-components'
+
+import { darken } from '@/main/utils'
+
+export const iconStyles: CSSProperties = {
+	alignSelf: 'center'
+}
+
+export const Container = styled.section`
+	display: flex;
+	flex-direction: column;
+
+	justify-content: center;
+	align-items: center;
+
+	background: ${({ theme }) =>
+		darken({ color: theme.colors.primary, percentage: 0.3 })};
+	min-height: 100vh;
+`
+
+export const Form = styled.form`
+	background: ${({ theme }) => theme.colors.white};
+	border-radius: 12px;
+
+	padding: 42px 64px;
+	width: 100%;
+	max-width: 480px;
+
+	display: flex;
+	flex-direction: column;
+
+	gap: 48px;
+
+	${({ theme }) => theme.media.forPhoneOnly()} {
+		padding: 32px 24px;
+
+		border-radius: 0;
+
+		height: 100%;
+		min-height: 100vh;
+	}
+`
+
+export const FormHeader = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	align-items: center;
+	justify-content: flex-start;
+
+	width: 100%;
+	gap: 4px;
+`
+
+export const FormBody = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	align-items: flex-start;
+	justify-content: flex-start;
+
+	width: 100%;
+	gap: 24px;
+	margin: auto 0;
+
+	${({ theme }) => theme.media.forPhoneOnly()} {
+		gap: 12px;
+	}
+`
+
+export const FormFooter = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+
+	width: 100%;
+	gap: 8px;
+	margin-top: auto;
+`
---
+++ src/presentation/templates/containers/auth/types.ts
@@ -0,0 +1,53 @@
+type Props = {
+	/**
+	 * The title of auth container
+	 *
+	 * @example
+	 * ```tsx
+	 * <AuthContainerTemplate title="Sign in" />
+	 * ```
+	 */
+	title: string
+
+	/**
+	 * The description of auth container
+	 *
+	 * @example
+	 * ```tsx
+	 * <AuthContainerTemplate description="Sign in to your account" />
+	 * ```
+	 */
+	description: string
+
+	/**
+	 * The main content of the form
+	 *
+	 * @example
+	 * ```tsx
+	 * <AuthContainerTemplate body={<h1>main content</h1>}
+	 * ```
+	 */
+	body: React.ReactNode
+
+	/**
+	 * The footer of the form
+	 *
+	 * @example
+	 * ```tsx
+	 * <AuthContainerTemplate footer={<h1>footer content</h1>}
+	 * ```
+	 */
+	footer: React.ReactNode
+
+	/**
+	 * The submit of the button
+	 *
+	 * @example
+	 * ```tsx
+	 * <AuthContainerTemplate handleSubmit={() => doSomething()} />
+	 * ```
+	 */
+	handleSubmit: () => Promise<void>
+}
+
+export type AuthContainerTemplateProps = Props
