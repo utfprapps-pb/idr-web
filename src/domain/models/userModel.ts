df374d7a1d2d9a60f9054601911847a2d85aa7a0
Guilherme Minozzi <minozzi60@gmail.com>
2023-03-31 19:43:37 -0300

    feat(userModel): add params to create user

--- src/domain/models/userModel.ts
+++ src/domain/models/userModel.ts
@@ -1,3 +1,12 @@
 export type UserModel = {
 	token: string
 }
+
+export type UserCreateParams = {
+	name: string
+	email: string
+	phone: string
+	graduationYear: string
+	professionalRegister: string
+	houseNumber: string
+}
