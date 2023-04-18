6403bba62138e75629ceadcc2bf353eda5dc5a28
Guilherme Minozzi <minozzi60@gmail.com>
2023-04-18 19:30:33 -0300

    feat(domain/models/userModel): add other props

--- src/domain/models/userModel.ts
+++ src/domain/models/userModel.ts
@@ -5,8 +5,14 @@ export type UserModel = {
 export type CreateUserModel = {
 	name: string
 	email: string
+	password: string
+	confirmPassword: string
+	cpf: string
 	phone: string
 	graduationYear: string
 	professionalRegister: string
+	cep: string
+	street: string
+	city: string
 	houseNumber: string
 }
