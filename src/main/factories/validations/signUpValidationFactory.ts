bc50130d9bc2356d86c124f3d5793dc04dea0970
Guilherme Minozzi <minozzi60@gmail.com>
2023-04-18 19:34:28 -0300

    feat(main/factories/validations): add other validations for the sign up page

--- src/main/factories/validations/signUpValidationFactory.ts
+++ src/main/factories/validations/signUpValidationFactory.ts
@@ -8,13 +8,25 @@ export const makeSignUpValidation = (): ValidationComposite =>
 	ValidationComposite.build([
 		...ValidationBuilder.field<ModelKeys>('name').required().build(),
 		...ValidationBuilder.field<ModelKeys>('email').required().email().build(),
+		...ValidationBuilder.field<ModelKeys>('password')
+			.required()
+			.password()
+			.build(),
+		...ValidationBuilder.field<ModelKeys>('confirmPassword')
+			.required()
+			.sameAs<ModelKeys>('password')
+			.build(),
+		...ValidationBuilder.field<ModelKeys>('cpf').required().cpf().build(),
 		...ValidationBuilder.field<ModelKeys>('phone').required().phone().build(),
-		...ValidationBuilder.field<ModelKeys>('houseNumber').required().build(),
 		...ValidationBuilder.field<ModelKeys>('graduationYear')
 			.required()
 			.year()
 			.build(),
 		...ValidationBuilder.field<ModelKeys>('professionalRegister')
 			.required()
-			.build()
+			.build(),
+		...ValidationBuilder.field<ModelKeys>('cep').required().build(),
+		...ValidationBuilder.field<ModelKeys>('street').required().build(),
+		...ValidationBuilder.field<ModelKeys>('city').required().build(),
+		...ValidationBuilder.field<ModelKeys>('houseNumber').required().build()
 	])
