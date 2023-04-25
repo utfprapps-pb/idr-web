e084a32b5c86c9243fb1116a46773cac11af4335
Minozzzi <Minozzzi@users.noreply.github.com>
2023-04-25 00:02:30 +0000

    Deploying to main from  @ 8f9c0fd3897ad4a7a9d6770ad26640c3b0778958 🚀

---
+++ docs/assets/index.stories.a7cb3277.js
@@ -0,0 +1,22 @@
+var r=Object.defineProperty;var o=(t,e)=>r(t,"name",{value:e,configurable:!0});import{P as n}from"./index.c350cac3.js";import{j as a}from"./jsx-runtime.02201e70.js";import"./index.b6821dff.js";import"./styled-components.browser.esm.016c4039.js";import"./reset.489d37f0.js";import"./index.27867f99.js";import"./iframe.8eb0eddc.js";const w={parameters:{storySource:{source:`import React from 'react'
+
+import { Meta, StoryFn } from '@storybook/react/'
+
+import { PasswordInput, PasswordInputProps } from '.'
+
+export default {
+	title: 'Components/Password Input',
+	component: PasswordInput
+} as Meta
+
+const Template: StoryFn<PasswordInputProps> = (args) => (
+	<PasswordInput {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+	label: 'Label Example',
+	onChange: () => null
+}
+`,locationsMap:{default:{startLoc:{col:46,line:12},endLoc:{col:1,line:14},startBody:{col:46,line:12},endBody:{col:1,line:14}}}}},title:"Components/Password Input",component:n},s=o(t=>a(n,{...t}),"Template"),p=s.bind({});p.args={label:"Label Example",onChange:()=>null};const x=["Default"];export{p as Default,x as __namedExportsOrder,w as default};
+//# sourceMappingURL=index.stories.a7cb3277.js.map
