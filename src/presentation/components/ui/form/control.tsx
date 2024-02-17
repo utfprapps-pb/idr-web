89fc99359cf50b9f969e20858d1667300f21cac7
Guilherme Minozzi <minozzi60@gmail.com>
2024-02-17 14:50:10 -0300

    feat(components/ui/form): add Control Component

---
+++ src/presentation/components/ui/form/control.tsx
@@ -0,0 +1,29 @@
+import React, { ComponentPropsWithRef } from 'react'
+
+import { Slot } from '@radix-ui/react-slot'
+
+import { useFormField } from './hooks/useFormField'
+import { ref } from 'yup'
+
+const Control: React.FC<ComponentPropsWithRef<typeof Slot>> = (
+	ref
+	...props,
+) => {
+	const { error, formItemId, formDescriptionId, formMessageId } = useFormField()
+
+	return (
+		<Slot
+			ref={ref}
+			id={formItemId}
+			aria-describedby={
+				!error
+					? `${formDescriptionId}`
+					: `${formDescriptionId} ${formMessageId}`
+			}
+			aria-invalid={!!error}
+			{...props}
+		/>
+	)
+}
+
+Control.displayName = 'FormControl'
