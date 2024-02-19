4ff79b58e2953a28e99ec5403cbcf26c2fb3a6cd
Guilherme Minozzi <minozzi60@gmail.com>
2024-02-19 08:37:38 -0300

    chore(components/ui/form): export form components

---
+++ src/presentation/components/ui/form/index.ts
@@ -0,0 +1,19 @@
+import { FormProvider } from 'react-hook-form'
+
+import { Control } from './control'
+import { Description } from './description'
+import { Field } from './field'
+import { Item } from './item'
+import { Label } from './label'
+import { Message } from './message'
+
+export { useFormField } from './hooks/useFormField'
+export const Form = {
+	Provider: FormProvider,
+	Control,
+	Description,
+	Field,
+	Item,
+	Label,
+	Message
+}
