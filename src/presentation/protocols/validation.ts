8fd86643856be59855c28a8217b31c4ab69a8470
Guilherme Minozzi <minozzi60@gmail.com>
2024-02-29 23:22:00 -0300

    feat: add interface for validationComposite

--- src/presentation/protocols/validation.ts
+++ src/presentation/protocols/validation.ts
@@ -1,3 +1,14 @@
+export type ErrorDef = {
+	[x: string]: { message: string }
+}
+
+type ValidateResponse = {
+	errors: ErrorDef
+	values: object
+}
+
+export type Validate = (props: { data: object }) => ValidateResponse
+
 export interface IValidation {
-	validate(params: { input: object; fieldName: string }): string | null
+	validate: Validate
 }
