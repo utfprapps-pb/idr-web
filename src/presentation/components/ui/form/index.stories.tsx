fe2666ebda0b2bf8e6f450b793efd0dee24f5ebd
Guilherme Minozzi <minozzi60@gmail.com>
2024-02-29 23:18:31 -0300

    feat: add form storybook

--- src/presentation/components/ui/form/index.stories.tsx
+++ src/presentation/components/ui/form/index.stories.tsx
@@ -1,8 +1,13 @@
 import { Meta, StoryFn } from '@storybook/react/'
-import { useForm } from 'react-hook-form'
+
+import { ValidationBuilder } from '@/main/builders'
+import { ValidationComposite } from '@/main/composite'
+import { schemaResolver } from '@/validation/schemaResolver'
 
 import { Button } from '../button'
 import { Input } from '../input'
+
+import { useHookForm } from './hooks/useHookForm'
 
 import { Form } from '.'
 
@@ -10,15 +15,46 @@ export default {
 	title: 'Components/UI/Form'
 } as Meta
 
+type FormData = {
+	name: string
+	email: string
+	password: string
+	passwordConfirmation: string
+}
+
 const Template: StoryFn = () => {
-	const form = useForm({
+	const schema = ValidationComposite.build([
+		...ValidationBuilder.field<keyof FormData>('name')
+			.required()
+			.min(3)
+			.build(),
+		...ValidationBuilder.field<keyof FormData>('email')
+			.required()
+			.email()
+			.build(),
+		...ValidationBuilder.field<keyof FormData>('password')
+			.required()
+			.min(20)
+			.email()
+			.build(),
+		...ValidationBuilder.field<keyof FormData>('passwordConfirmation')
+			.required()
+			.sameAs('password')
+			.build()
+	])
+
+	const form = useHookForm<FormData>({
 		defaultValues: {
-			name: ''
-		}
+			name: '',
+			email: '',
+			password: '',
+			passwordConfirmation: ''
+		},
+		schemaResolver: (data) => schemaResolver({ data, schema })
 	})
 
-	const onSubmit = (data: any) => {
-		alert(JSON.stringify(data, null, 2))
+	const onSubmit = (data: FormData) => {
+		console.info('formData: ', { ...data })
 	}
 
 	return (
@@ -39,6 +75,50 @@ const Template: StoryFn = () => {
 					)}
 				/>
 
+				<Form.Field
+					control={form.control}
+					name="email"
+					render={({ field }) => (
+						<Form.Item>
+							<Form.Label>Email</Form.Label>
+							<Form.Control>
+								<Input placeholder="Email" {...field} />
+							</Form.Control>
+							<Form.Description>Digite seu email</Form.Description>
+							<Form.Message />
+						</Form.Item>
+					)}
+				/>
+
+				<Form.Field
+					control={form.control}
+					name="password"
+					render={({ field }) => (
+						<Form.Item>
+							<Form.Label>Senha</Form.Label>
+							<Form.Control>
+								<Input placeholder="Senha" {...field} />
+							</Form.Control>
+							<Form.Description>Digite sua senha</Form.Description>
+							<Form.Message />
+						</Form.Item>
+					)}
+				/>
+
+				<Form.Field
+					control={form.control}
+					name="passwordConfirmation"
+					render={({ field }) => (
+						<Form.Item>
+							<Form.Label>Confirme sua senha</Form.Label>
+							<Form.Control>
+								<Input placeholder="Confirmar senha" {...field} />
+							</Form.Control>
+							<Form.Description>Digite novamente sua senha</Form.Description>
+							<Form.Message />
+						</Form.Item>
+					)}
+				/>
 				<Button type="submit">Enviar</Button>
 			</form>
 		</Form.Provider>
