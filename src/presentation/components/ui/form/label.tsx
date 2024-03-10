39e65ad6e89c8ec2e1fbd67cd883ffe230c26e82
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-10 00:01:59 -0300

    fix(components/label): no cycle on import and remove text-destructive class

--- src/presentation/components/ui/form/label.tsx
+++ src/presentation/components/ui/form/label.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import * as LabelPrimitive from '@radix-ui/react-label'
 
 import { cn } from '@/main/utils'
-import { Label as AppLabel } from '@/presentation/components/ui'
+
+import { Label as AppLabel } from '../label'
 
 import { useFormField } from './hooks/useFormField'
 
@@ -11,12 +12,12 @@ export const Label = React.forwardRef<
 	React.ElementRef<typeof LabelPrimitive.Root>,
 	React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => {
-	const { error, formItemId } = useFormField()
+	const { formItemId } = useFormField()
 
 	return (
 		<AppLabel
 			ref={ref}
-			className={cn(error && 'text-destructive', className)}
+			className={cn(className)}
 			htmlFor={formItemId}
 			{...props}
 		/>
