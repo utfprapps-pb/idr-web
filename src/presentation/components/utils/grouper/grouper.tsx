38ba69779cfd85e9c9494d679adc606893fc7495
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-10 00:03:44 -0300

    fix(components/utils/grouper): grid cols when is mobile

--- src/presentation/components/utils/grouper/grouper.tsx
+++ src/presentation/components/utils/grouper/grouper.tsx
@@ -1,13 +1,16 @@
 import { Children } from 'react'
 
 import { cn } from '@/main/utils'
+import { useWindowResize } from '@/presentation/hooks/useWindowResize'
 
 import { styles } from './styles'
 
 import type { GrouperProps } from './types'
 
 const Grouper: React.FC<GrouperProps> = ({ children, className, ...props }) => {
-	const gridCols = Children.count(children)
+	const { width } = useWindowResize()
+
+	const gridCols = width >= 640 ? Children.count(children) : 1
 
 	return (
 		<section
--- src/presentation/components/utils/grouper/styles.ts
+++ src/presentation/components/utils/grouper/styles.ts
@@ -1,7 +1,3 @@
 import { ClassValue } from 'clsx'
 
-export const styles: ClassValue[] = [
-	'grid',
-	'gap-4 w-full',
-	'max-sm:grid-cols-1'
-]
+export const styles: ClassValue[] = ['grid', 'gap-4 w-full']
