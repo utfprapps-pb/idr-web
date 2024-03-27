b90f02268098492162d529865a1f41a9c9d075c9
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-26 21:56:15 -0300

    feat: add factory for loggedContainer

--- src/main/proxies/privateRouteProxy.tsx
+++ src/main/proxies/privateRouteProxy.tsx
@@ -2,9 +2,9 @@ import { PropsWithChildren } from 'react'
 
 import { Navigate } from 'react-router-dom'
 
-import { LoggedContainer } from '@/presentation/containers'
 import { useAuth } from '@/presentation/store'
 
+import { MakeLoggedContainer } from '../factories/containers'
 import { PAGE_PATHS } from '../routes/paths'
 
 export const PrivateRouteProxy: React.FC<PropsWithChildren> = ({
@@ -12,7 +12,7 @@ export const PrivateRouteProxy: React.FC<PropsWithChildren> = ({
 }) => {
 	const { auth } = useAuth()
 
-	if (auth?.token) return <LoggedContainer>{children}</LoggedContainer>
+	if (auth?.token) return <MakeLoggedContainer>{children}</MakeLoggedContainer>
 
 	return <Navigate to={PAGE_PATHS.login} />
 }
