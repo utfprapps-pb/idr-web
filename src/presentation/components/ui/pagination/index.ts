1cd002c625928f485d3ad364b89e42bea1d354fe
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-28 14:49:26 -0300

    feat: add pagination

---
+++ src/presentation/components/ui/pagination/index.ts
@@ -0,0 +1,17 @@
+import { Content } from './content'
+import { Ellipsis } from './ellipsis'
+import { Item } from './item'
+import { Link } from './link'
+import { Next } from './next'
+import { Root } from './pagination'
+import { Previous } from './previous'
+
+export const Pagination = {
+	Root,
+	Content,
+	Ellipsis,
+	Item,
+	Link,
+	Next,
+	Previous
+}
