99f7a3ee193ed40010ac794b57cc0f102d1972f8
Minozzzi <Minozzzi@users.noreply.github.com>
2024-03-28 17:57:07 +0000

    Deploying to main from  @ c7fc729cd65e31bd2a2b5c9ecd75e17a763dd63c 🚀

---
+++ storybook-docs/assets/chevron-right-f29811ac.js
@@ -0,0 +1,6 @@
+import{c as t}from"./createLucideIcon-0bafd447.js";/**
+ * @license lucide-react v0.320.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
