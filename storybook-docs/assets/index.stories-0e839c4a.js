413d8d8a8a159e768eb3b9b5485c8d52abc03b5c
Minozzzi <Minozzzi@users.noreply.github.com>
2024-03-28 16:38:10 +0000

    Deploying to main from  @ 0567c4da97c2739a2858c44ec28349264282f954 🚀

---
+++ storybook-docs/assets/index.stories-0e839c4a.js
@@ -0,0 +1,19 @@
+import{j as e}from"./jsx-runtime-ffb262ed.js";import{T as a}from"./index-d42dfe0f.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./index-1abfe904.js";const b={title:"Components/UI/Table"},o=()=>e.jsxs(a.Root,{children:[e.jsx(a.Caption,{children:"A list of your recent invoices."}),e.jsx(a.Header,{children:e.jsxs(a.Row,{children:[e.jsx(a.Head,{className:"w-[100px]",children:"Invoice"}),e.jsx(a.Head,{children:"Status"}),e.jsx(a.Head,{children:"Method"}),e.jsx(a.Head,{className:"text-right",children:"Amount"})]})}),e.jsx(a.Body,{children:e.jsxs(a.Row,{children:[e.jsx(a.Cell,{className:"font-medium",children:"INV001"}),e.jsx(a.Cell,{children:"Paid"}),e.jsx(a.Cell,{children:"Credit Card"}),e.jsx(a.Cell,{className:"text-right",children:"$250.00"})]})})]}),l=o.bind({});var s,t,n;l.parameters={...l.parameters,docs:{...(s=l.parameters)==null?void 0:s.docs,source:{originalSource:`() => <Table.Root>
+        <Table.Caption>A list of your recent invoices.</Table.Caption>
+        <Table.Header>
+            <Table.Row>
+                <Table.Head className="w-[100px]">Invoice</Table.Head>
+                <Table.Head>Status</Table.Head>
+                <Table.Head>Method</Table.Head>
+                <Table.Head className="text-right">Amount</Table.Head>
+            </Table.Row>
+        </Table.Header>
+        <Table.Body>
+            <Table.Row>
+                <Table.Cell className="font-medium">INV001</Table.Cell>
+                <Table.Cell>Paid</Table.Cell>
+                <Table.Cell>Credit Card</Table.Cell>
+                <Table.Cell className="text-right">$250.00</Table.Cell>
+            </Table.Row>
+        </Table.Body>
+    </Table.Root>`,...(n=(t=l.parameters)==null?void 0:t.docs)==null?void 0:n.source}}};const m=["Default"];export{l as Default,m as __namedExportsOrder,b as default};
