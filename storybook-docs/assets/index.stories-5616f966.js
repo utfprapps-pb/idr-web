47f829d87a3bcde078f38c2ca7d5cddb35c4b0d8
Minozzzi <Minozzzi@users.noreply.github.com>
2024-03-30 19:44:56 +0000

    Deploying to main from  @ b69ca1d9d60e373fe402b3b09ef13ade3dec0a17 🚀

---
+++ storybook-docs/assets/index.stories-5616f966.js
@@ -0,0 +1,11 @@
+import{j as r}from"./jsx-runtime-ffb262ed.js";import{A as s}from"./index-514cd920.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./index-7fe56c00.js";import"./index-dc576e2e.js";import"./index-d3ea75b5.js";import"./index-38dc68cb.js";import"./index-1abfe904.js";const x={title:"Components/UI/Avatar"},n=({isFallback:p})=>r.jsxs(s.Root,{children:[r.jsx(s.Image,{src:p?"":"https://avatars.githubusercontent.com/u/61752597?v=4",alt:"GitHub Profile Image"}),r.jsx(s.Fallback,{children:"GM"})]}),a=n.bind({});a.args={isFallback:!1};const t=n.bind({});t.args={isFallback:!0};var o,e,c;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  isFallback
+}) => <Avatar.Root>
+        <Avatar.Image src={isFallback ? '' : 'https://avatars.githubusercontent.com/u/61752597?v=4'} alt="GitHub Profile Image" />
+        <Avatar.Fallback>GM</Avatar.Fallback>
+    </Avatar.Root>`,...(c=(e=a.parameters)==null?void 0:e.docs)==null?void 0:c.source}}};var i,l,m;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  isFallback
+}) => <Avatar.Root>
+        <Avatar.Image src={isFallback ? '' : 'https://avatars.githubusercontent.com/u/61752597?v=4'} alt="GitHub Profile Image" />
+        <Avatar.Fallback>GM</Avatar.Fallback>
+    </Avatar.Root>`,...(m=(l=t.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const f=["AvatarWithImage","AvatarWithFallback"];export{t as AvatarWithFallback,a as AvatarWithImage,f as __namedExportsOrder,x as default};
