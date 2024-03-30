b21790f9455bebfae9f0f8be35693acfbe1fa289
Minozzzi <Minozzzi@users.noreply.github.com>
2024-03-30 13:42:13 +0000

    Deploying to main from  @ 28395f3a5836a991e67d714371fa7fb78b819574 🚀

--- storybook-docs/assets/index.stories-9213669d.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-ffb262ed.js";import{T as r}from"./index-d6d4f6fe.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./extends-98964cd2.js";import"./index-40b49654.js";import"./index-dc576e2e.js";import"./index-d3ea75b5.js";import"./index-38dc68cb.js";import"./index-45e22a90.js";import"./index-1abfe904.js";const f={title:"Components/UI/Tooltip"},n=()=>o.jsx(r.Provider,{children:o.jsxs(r.Root,{children:[o.jsx(r.Trigger,{children:"Hover"}),o.jsx(r.Content,{children:o.jsx("p",{children:"Add to library"})})]})}),t=n.bind({});var i,e,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`() => <Tooltip.Provider>
-        <Tooltip.Root>
-            <Tooltip.Trigger>Hover</Tooltip.Trigger>
-            <Tooltip.Content>
-                <p>Add to library</p>
-            </Tooltip.Content>
-        </Tooltip.Root>
-    </Tooltip.Provider>`,...(p=(e=t.parameters)==null?void 0:e.docs)==null?void 0:p.source}}};const h=["Default"];export{t as Default,h as __namedExportsOrder,f as default};
