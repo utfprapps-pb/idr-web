ab8f9fd302896d5af9dd781a51456fd5fb2046ea
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-30 16:28:02 -0300

    refactor(components/ui/label): keep default patterns

--- src/presentation/components/ui/label/types.ts
+++ src/presentation/components/ui/label/types.ts
@@ -1,12 +1,8 @@
-import React from 'react'
-
-import * as LabelPrimitive from '@radix-ui/react-label'
+import { Root } from '@radix-ui/react-label'
 import { VariantProps } from 'class-variance-authority'
 
 import { labelVariants } from './styles'
 
-export type LabelElement = React.ElementRef<typeof LabelPrimitive.Root>
-export type LabelProps = React.ComponentPropsWithoutRef<
-	typeof LabelPrimitive.Root
-> &
+export type LabelElement = React.ElementRef<typeof Root>
+export type LabelProps = React.ComponentPropsWithoutRef<typeof Root> &
 	VariantProps<typeof labelVariants>
