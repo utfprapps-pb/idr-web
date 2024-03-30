24434f96ce4c42bd9cbaae987dd65d6cc771fd9a
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-30 10:38:07 -0300

    style: add spacings in Sheet.Footer

--- src/presentation/components/ui/sheet/footer.tsx
+++ src/presentation/components/ui/sheet/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
 	<div
 		className={cn(
-			'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+			'flex flex-col-reverse gap-4 mt-auto sm:flex-row sm:justify-center',
 			className
 		)}
 		{...props}
