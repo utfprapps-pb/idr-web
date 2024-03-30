2590d7550c4c0426dc512a37737564bbcdc09bb7
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-29 23:09:52 -0300

    feat: add sheet component

---
+++ src/presentation/components/ui/sheet/header.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/main/utils'
+
+export const Header = ({
+	className,
+	...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+	<div
+		className={cn(
+			'flex flex-col space-y-2 text-center sm:text-left',
+			className
+		)}
+		{...props}
+	/>
+)
+
+Header.displayName = 'SheetHeader'
