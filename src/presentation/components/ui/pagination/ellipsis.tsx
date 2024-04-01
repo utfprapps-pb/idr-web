e6ed549768c61d0fda86640adf97192c32b8f412
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-01 14:30:00 -0300

    fix: pagination ellipsis broken screen

--- src/presentation/components/ui/pagination/ellipsis.tsx
+++ src/presentation/components/ui/pagination/ellipsis.tsx
@@ -2,13 +2,8 @@ import { MoreHorizontal } from 'lucide-react'
 
 import { cn } from '@/main/utils'
 
-type EllipsisProps = React.ComponentProps<'span'> & {
-	text?: string
-}
-
-export const Ellipsis: React.FC<EllipsisProps> = ({
+export const Ellipsis: React.FC<React.ComponentProps<'span'>> = ({
 	className,
-	text = 'Mais páginas',
 	...props
 }) => (
 	<span
@@ -16,8 +11,7 @@ export const Ellipsis: React.FC<EllipsisProps> = ({
 		className={cn('flex h-9 w-9 items-center justify-center', className)}
 		{...props}
 	>
-		<MoreHorizontal className="h-4 w-4" />
-		<span className="sr-only">{text}</span>
+		<MoreHorizontal size={16} />
 	</span>
 )
 
