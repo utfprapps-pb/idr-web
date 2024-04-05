42bd875b0f3b41bb236a818f0851f8fbd47fc8af
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-05 19:33:27 -0300

    feat: add msw middlewares

---
+++ src/mocks/middleware/withAuth.ts
@@ -0,0 +1,27 @@
+import {
+	DefaultBodyType,
+	HttpResponse,
+	PathParams,
+	type HttpResponseResolver
+} from 'msw'
+import { type AsyncResponseResolverReturnType } from 'msw/lib/core/handlers/RequestHandler'
+
+export function withAuth<
+	Params extends PathParams,
+	RequestBodyType extends DefaultBodyType,
+	ResponseBodyType extends DefaultBodyType
+>(
+	resolver: HttpResponseResolver<Params, RequestBodyType, ResponseBodyType>
+): HttpResponseResolver<Params, RequestBodyType, ResponseBodyType> {
+	return (context) => {
+		const { request } = context
+
+		if (!request.headers.get('Authorization')) {
+			return new HttpResponse(null, {
+				status: 401
+			}) as AsyncResponseResolverReturnType<ResponseBodyType>
+		}
+
+		return resolver(context)
+	}
+}
---
+++ src/mocks/middleware/withDelay.ts
@@ -0,0 +1,11 @@
+import { HttpResponseResolver, delay } from 'msw'
+
+const ONE_SECOND = 1000
+
+export function withDelay(durationMs = ONE_SECOND) {
+	return (resolver: HttpResponseResolver): HttpResponseResolver =>
+		async (...args) => {
+			await delay(durationMs)
+			return resolver(...args)
+		}
+}
