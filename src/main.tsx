44600be0e8154ae3d92ed41b7db35d1e8e3fb917
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-12 19:14:00 -0300

    feat(main): use mocks according env

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,23 @@ import { BrowserRouter } from 'react-router-dom'
 import { Compose } from '@/presentation/components/utils'
 
 import { App } from './App'
+import { env } from './main/env'
 import { AuthProvider } from './presentation/store'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-	<StrictMode>
-		<BrowserRouter>
-			<Compose components={[AuthProvider]}>
-				<App />
-			</Compose>
-		</BrowserRouter>
-	</StrictMode>
-)
+async function bootstrap() {
+	if (env.VITE_API_MOCKED) {
+		const { worker } = await import('./mocks/browser')
+		worker.start()
+	}
+
+	createRoot(document.getElementById('root') as HTMLElement).render(
+		<StrictMode>
+			<BrowserRouter>
+				<Compose components={[AuthProvider]}>
+					<App />
+				</Compose>
+			</BrowserRouter>
+		</StrictMode>
+	)
+}
+bootstrap()
