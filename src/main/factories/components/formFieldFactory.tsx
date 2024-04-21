8824852d484fed92de742a95cc673c816b66730d
Guilherme Minozzi <minozzi60@gmail.com>
2024-04-21 14:13:41 -0300

    refactor(main/factories/components/formFieldFactory): use renderProps function

--- src/main/factories/components/formFieldFactory.tsx
+++ src/main/factories/components/formFieldFactory.tsx
@@ -1,44 +1,62 @@
-import { FieldPath, FieldValues } from 'react-hook-form'
+import {
+	type ControllerFieldState,
+	type ControllerRenderProps,
+	type FieldPath,
+	type FieldValues,
+	type UseFormStateReturn
+} from 'react-hook-form'
 
-import { Form, Input, type InputProps } from '@/presentation/components/ui'
-import { useHookForm } from '@/presentation/components/ui/form/hooks/useHookForm'
+import { Form } from '@/presentation/components/ui'
 import { Grouper } from '@/presentation/components/utils'
+import { useHookForm } from '@/presentation/hooks/useHookForm'
 
-export type InputData<T extends FieldValues> = InputProps & {
-	name: FieldPath<T>
+type RenderComponentProps<
+	TFieldValues extends FieldValues,
+	TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {
+	field: ControllerRenderProps<TFieldValues, TName>
+	fieldState: ControllerFieldState
+	formState: UseFormStateReturn<TFieldValues>
+}
+
+export type ComponentProps<TFieldValues extends FieldValues> = {
+	name: FieldPath<TFieldValues>
 	label?: string
+	renderComponent: (
+		props: RenderComponentProps<TFieldValues, FieldPath<TFieldValues>>
+	) => React.ReactNode
 }
 
-export type InputDataGrouped<T extends FieldValues> = {
+export type InputDataGrouped<TFieldValues extends FieldValues> = {
 	key: string
-	group: InputData<T>[]
+	group: ComponentProps<TFieldValues>[]
 }
 
-type FormFieldFactoryProps<T extends FieldValues> = {
-	form: ReturnType<typeof useHookForm<T>>
-	inputData: InputDataGrouped<T>[]
+type FormFieldFactoryProps<TFieldValues extends FieldValues> = {
+	form: ReturnType<typeof useHookForm<TFieldValues>>
+	inputData: InputDataGrouped<TFieldValues>[]
 }
 
-export const FormFieldFactory = <T extends FieldValues>({
+export const FormFieldFactory = <TFieldValues extends FieldValues>({
 	form,
 	inputData
-}: FormFieldFactoryProps<T>) =>
+}: FormFieldFactoryProps<TFieldValues>) =>
 	inputData.map(({ key, group }) => (
 		<Grouper key={key}>
-			{group.map(({ name, label, ...props }) => (
+			{group.map(({ name, label, renderComponent }) => (
 				<Form.Field
 					key={name}
 					control={form.control}
 					name={name}
-					render={({ field }) => (
+					render={({ field, fieldState, formState }) => (
 						<Form.Item>
 							{label ? <Form.Label>{label}</Form.Label> : null}
 							<Form.Control>
-								<Input
-									{...field}
-									{...props}
-									isError={!!form.formState.errors[name]}
-								/>
+								{renderComponent({
+									field,
+									fieldState,
+									formState
+								})}
 							</Form.Control>
 							<Form.Message />
 						</Form.Item>
