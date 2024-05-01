b8ea9ebb63b5ae8646654e985900d23ee0e90c57
Guilherme Minozzi <minozzi60@gmail.com>
2024-05-01 20:00:00 -0300

    feat(mocks/shared/filterData): validate if key is valid

--- src/mocks/shared/filterData.ts
+++ src/mocks/shared/filterData.ts
@@ -6,6 +6,11 @@ export const filterData = <TData extends Record<string, string>>(
 
 	return data.filter((item) =>
 		filterEntries.every(([key, value]) => {
+			const keyIn = key in item
+			if (!keyIn) {
+				return false
+			}
+
 			const itemValue = item[key as keyof TData]
 			return itemValue.toLowerCase().includes(value.toLowerCase())
 		})
