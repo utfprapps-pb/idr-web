9cc906c04a847817a9ed8c5ba0b5a336d44ba567
Guilherme Minozzi <minozzi60@gmail.com>
2024-05-02 16:31:00 -0300

    refactor(main/factories/useCases/user): import domain module as types

--- src/main/factories/useCases/user/remoteCreateUserFactory.ts
+++ src/main/factories/useCases/user/remoteCreateUserFactory.ts
@@ -1,6 +1,7 @@
 import { RemoteCreateUser } from '@/data/useCases'
-import { ICreateUser } from '@/domain/useCases'
 import { makeApiHttpClient } from '@/main/factories/http'
+
+import type { ICreateUser } from '@/domain/useCases'
 
 export const makeRemoteCreateUser = (): ICreateUser =>
 	new RemoteCreateUser('users', makeApiHttpClient<void>())
--- src/main/factories/useCases/user/remoteLoginFactory.ts
+++ src/main/factories/useCases/user/remoteLoginFactory.ts
@@ -1,7 +1,8 @@
 import { RemoteLoginUser } from '@/data/useCases/'
-import { UserModel } from '@/domain/models'
-import { ILoginUser } from '@/domain/useCases'
 import { makeApiHttpClient } from '@/main/factories/http'
+
+import type { UserModel } from '@/domain/models'
+import type { ILoginUser } from '@/domain/useCases'
 
 export const makeRemoteLoginUser = (): ILoginUser =>
 	new RemoteLoginUser('login', makeApiHttpClient<UserModel>())
