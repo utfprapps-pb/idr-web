32f95033dac4b41849659eb37ece49f56b82b9aa
Guilherme Minozzi <minozzi60@gmail.com>
2024-05-02 14:31:00 -0300

    refactor(main/factories/useCases/cep): import domain module as types

--- src/main/factories/useCases/cep/remoteGetCepFactory.ts
+++ src/main/factories/useCases/cep/remoteGetCepFactory.ts
@@ -1,7 +1,8 @@
 import { RemoteGetCep } from '@/data/useCases'
-import { CepModel } from '@/domain/models'
-import { IGetCep } from '@/domain/useCases'
 import { makeBrasilApiHttpClient } from '@/main/factories/http'
+
+import type { CepModel } from '@/domain/models'
+import type { IGetCep } from '@/domain/useCases'
 
 export const makeRemoteGetCep = (): IGetCep =>
 	new RemoteGetCep('/cep/v1', makeBrasilApiHttpClient<CepModel>())
