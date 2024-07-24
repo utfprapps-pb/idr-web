982a378fbe92a1a12b305d331697e07d314e03d3
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2024-07-24 12:03:21 -0300

    fix(mocks/modules/property/services/getPropertiesService): total registers

--- src/mocks/modules/property/services/getPropertiesService.ts
+++ src/mocks/modules/property/services/getPropertiesService.ts
@@ -62,7 +62,7 @@ export const getPropertiesService = httpWithMiddleware<never, Params, Response>(
 			return HttpResponse.json(
 				{
 					properties: filteredData,
-					totalRegisters: filteredData.length
+					totalRegisters: propertiesData.length
 				},
 				{ status: HttpStatusCode.ok }
 			)
