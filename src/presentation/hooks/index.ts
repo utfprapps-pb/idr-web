37d07ab801826274b34eb6f078a89ccff41c3146
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2024-07-24 12:01:55 -0300

    chore: remove use modal hook

--- src/presentation/hooks/index.ts
+++ src/presentation/hooks/index.ts
@@ -1,7 +1,5 @@
 export * from './useDebounce'
 export * from './useHookForm'
 export * from './useIdrHistory'
-export * from './usePageContainer'
-export * from './useModal'
 export * from './useTimeout'
 export * from './useWindowResize'
