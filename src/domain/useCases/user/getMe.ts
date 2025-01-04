d2315b588d91aeb8a1234cf726701ba5f137542b
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2025-01-04 12:32:53 -0300

    refactor(domain/useCases): improve nomenclatures

--- src/domain/useCases/user/getMe.ts
+++ src/domain/useCases/user/getMe.ts
@@ -1,4 +1,4 @@
 import type { UserModel } from '@/domain/models/userModel'
 import type { IRequestInterface } from '@/domain/shared/types'
 
-export type IMeUser = IRequestInterface<void, UserModel>
+export type IGetMe = IRequestInterface<void, UserModel>
