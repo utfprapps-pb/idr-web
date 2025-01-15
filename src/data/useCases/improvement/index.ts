bb04797008573ef332bb88febb8363dbc8e5dedc
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2025-01-15 08:50:59 -0300

    feat(data): implement improvements model use cases

---
+++ src/data/useCases/improvement/index.ts
@@ -0,0 +1,13 @@
+import { RemoteCreateImprovement } from './remoteCreateImprovement'
+import { RemoteDeleteImprovement } from './remoteDeleteImprovement'
+import { RemoteGetImprovement } from './remoteGetImprovement'
+import { RemoteGetImprovements } from './remoteGetImprovements'
+import { RemoteUpdateImprovement } from './remoteUpdateForage'
+
+export const ImprovementData = {
+  RemoteCreateImprovement,
+  RemoteDeleteImprovement,
+  RemoteGetImprovement,
+  RemoteGetImprovements,
+  RemoteUpdateImprovement,
+}
