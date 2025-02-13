8f704cec397ebc80f3ddebdc4146bd6a8cd768c4
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2025-02-12 21:12:56 -0300

    fix(data/useCases/improvement): use correct file name

--- src/data/useCases/improvement/index.ts
+++ src/data/useCases/improvement/index.ts
@@ -2,7 +2,7 @@ import { RemoteCreateImprovement } from './remoteCreateImprovement'
 import { RemoteDeleteImprovement } from './remoteDeleteImprovement'
 import { RemoteGetImprovement } from './remoteGetImprovement'
 import { RemoteGetImprovements } from './remoteGetImprovements'
-import { RemoteUpdateImprovement } from './remoteUpdateForage'
+import { RemoteUpdateImprovement } from './remoteUpdateImprovement'
 
 export const ImprovementData = {
   RemoteCreateImprovement,
