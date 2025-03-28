83c48d29af33d2b13042c87bb9c5fe7e07df7776
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2025-03-28 19:56:34 -0300

    feat: add animal heifer calf stage domain model

---
+++ src/app/modules/animals/domain/models/animal-heifer-calf-stages-model.ts
@@ -0,0 +1,59 @@
+import type { WithId } from '@/core/domain/types'
+
+type GMDStatus = 'normal' | 'overweight' | 'underweight'
+
+type ReproductionStatus = 'fit' | 'unfit'
+
+export type AnimalHeiferCalfStageDetailsModel = {
+  weighingDate: Date
+  ecc: string
+  age: {
+    years: string
+    months: string
+  }
+  weighing: {
+    last: string
+    current: string
+  }
+  ageWeightEstimate: {
+    last: string
+    current: string
+  }
+  gmd: {
+    min: string
+    max: string
+    real: string
+    status: GMDStatus
+  }
+  amountOfMilk: {
+    correction: string
+    morning: string
+    afternoon: string
+  }
+  weaningDate: {
+    first: string
+    second: string
+  }
+  removeLittleHouseDate: string
+  amountOfEstimateConcentrate: {
+    correction: string
+    heifer: string
+    calf: string
+  }
+  bulky: string
+  dateToProvideSilage: string
+  reproduction: {
+    status: ReproductionStatus
+    minWeight: string
+    fromDate: string
+    carriedOut: string
+    artificialInseminationNumber: string
+  }
+}
+
+export type AnimalHeiferCalfStageModel = WithId<{
+  weighingDate: string
+  weight: string
+  ecc: string
+  age: string
+}>
