cde80942d5c2bf239b14c6be4bb01fc600c3f72d
Guilherme Minozzi <minozzi60@gmail.com>
2025-04-09 22:10:16 -0300

    chore: rename folder to keep name pattern

--- src/app/modules/animals/main/factories/use-cases/animal-heifer-calf-stages-use-cases/remote-create-animal-heifer-calf-stage-use-case-factory.ts
+++ src/app/modules/animals/main/factories/use-cases/animal-heifer-calf-stages-use-cases/remote-create-animal-heifer-calf-stage-use-case-factory.ts
@@ -1,6 +1,6 @@
 import { makeApiHttpClient } from '@/core/main/factories/http'
 
-import { RemoteCreateAnimalHeiferCalfStageUseCase } from '../../../../data/use-cases/heifer-calf-stages-use-cases'
+import { RemoteCreateAnimalHeiferCalfStageUseCase } from '../../../../data/use-cases/animal-heifer-calf-stages-use-cases'
 
 import type { CreateAnimalHeiferCalfStageUseCase } from '../../../../domain/use-cases/animal-heifer-calf-stages-use-cases'
 
