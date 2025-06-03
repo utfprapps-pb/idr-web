ca128f139e0f6e696cc1b04384b514b085f47e7f
Guilherme Minozzi <minozzi60@gmail.com>
2025-06-03 20:22:24 -0300

    refactor(modules/improvements/main/factories): use correctly props from backend on listing

--- src/app/modules/improvements/main/factories/use-cases/remote-get-improvements-use-case-factory.ts
+++ src/app/modules/improvements/main/factories/use-cases/remote-get-improvements-use-case-factory.ts
@@ -2,12 +2,20 @@ import { makeApiHttpClient } from '@/core/main/factories/http'
 
 import { RemoteGetImprovementsUseCase } from '../../../data/use-cases'
 
-import type { ImprovementModel } from '../../../domain/models/improvements-model'
+import type {
+  ImprovementApiResponse,
+  ImprovementModel,
+} from '../../../domain/models/improvements-model'
 import type { GetImprovementsUseCase } from '../../../domain/use-cases'
+import type { ListApiResponse } from '@/core/domain/types'
 
 export function makeRemoteGetImprovementsUseCase(): GetImprovementsUseCase {
   return new RemoteGetImprovementsUseCase(
     'properties/:propertyId/improvements',
-    makeApiHttpClient<ImprovementModel[]>()
+    makeApiHttpClient<
+      ImprovementModel,
+      ImprovementApiResponse,
+      ListApiResponse<ImprovementApiResponse[]>
+    >()
   )
 }
