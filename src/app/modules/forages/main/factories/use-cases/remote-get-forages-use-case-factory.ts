8ee611198d98687ad2dfd88e3efc9aef399aa1e3
Guilherme Minozzi <minozzi60@gmail.com>
2025-06-08 20:32:39 -0300

    refactor(modules/forages/main/factories): use correctly props from backend on listing

--- src/app/modules/forages/main/factories/use-cases/remote-get-forages-use-case-factory.ts
+++ src/app/modules/forages/main/factories/use-cases/remote-get-forages-use-case-factory.ts
@@ -2,12 +2,20 @@ import { makeApiHttpClient } from '@/core/main/factories/http'
 
 import { RemoteGetForagesUseCase } from '../../../data/use-cases'
 
-import type { ForageModel } from '../../../domain/models/forages-model'
+import type {
+  ForageApiResponse,
+  ForageModel,
+} from '../../../domain/models/forages-model'
 import type { GetForagesUseCase } from '../../../domain/use-cases'
+import type { ListApiResponse } from '@/core/domain/types'
 
 export function makeRemoteGetForagesUseCase(): GetForagesUseCase {
   return new RemoteGetForagesUseCase(
     'properties/:propertyId/forages',
-    makeApiHttpClient<ForageModel[]>()
+    makeApiHttpClient<
+      ForageModel,
+      ForageApiResponse,
+      ListApiResponse<ForageApiResponse[]>
+    >()
   )
 }
