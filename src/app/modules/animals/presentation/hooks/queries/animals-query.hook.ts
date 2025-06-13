036d360c49ba6edd62185ca984f6aafbdc8ebf70
Guilherme Minozzi <minozzi60@gmail.com>
2025-06-13 20:42:57 -0300

    refactor(modules/animals/presentation/hooks): use correctly props from backend on listing

--- src/app/modules/animals/presentation/hooks/queries/animals-query.hook.ts
+++ src/app/modules/animals/presentation/hooks/queries/animals-query.hook.ts
@@ -20,6 +20,7 @@ export function useAnimalsQuery({ propertyId, filters, page, sort }: Props) {
   const {
     data,
     isError,
+    error,
     isLoading,
     refetch: refetchAnimals,
   } = useQuery({
@@ -27,17 +28,15 @@ export function useAnimalsQuery({ propertyId, filters, page, sort }: Props) {
     queryFn: () =>
       getAnimalsUseCase.execute({
         propertyId,
-        queryParams: {
-          pagination: { page },
-          sort,
-          filters,
-        },
+        pagination: { page },
+        sort,
+        filters,
       }),
   })
 
   useEffect(() => {
-    if (isError) toast.error('Erro ao buscar animais')
-  }, [isError])
+    if (isError) toast.error(error?.message ?? 'Erro ao buscar animais')
+  }, [error, isError])
 
   return {
     animals: data ?? {
