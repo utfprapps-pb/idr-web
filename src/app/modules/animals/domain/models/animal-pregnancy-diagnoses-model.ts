feec34f22bc7752fbab80d4576b880de469720ee
Guilherme Minozzi <minozzi60@gmail.com>
2025-06-23 21:19:04 -0300

    feat(modules/animals/pregnancy-diagnoses): addapi response model for animal pregnancy details

--- src/app/modules/animals/domain/models/animal-pregnancy-diagnoses-model.ts
+++ src/app/modules/animals/domain/models/animal-pregnancy-diagnoses-model.ts
@@ -5,9 +5,17 @@ export type AnimalPregnancyDiagnosisDetailsModel = {
   lastInseminationDate: Date
 }
 
+export type AnimalPregnancyDiagnosisDetailsApiResponse = {
+  date: string
+  lastInseminationDate: string
+}
+
 export type AnimalPregnancyDiagnosisModel = WithId<{
+  date: Date
+  lastInseminationDate: Date
+}>
+
+export type AnimalPregnancyDiagnosisApiResponse = WithId<{
   date: string
   lastInseminationDate: string
 }>
-
-export type AnimalPregnancyDiagnosisApiResponse = AnimalPregnancyDiagnosisModel
