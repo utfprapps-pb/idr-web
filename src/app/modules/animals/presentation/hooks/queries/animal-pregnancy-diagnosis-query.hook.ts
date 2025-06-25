c939b49b0a6c946fe6c5579cf120fab09c45b71d
Guilherme Minozzi <minozzi60@gmail.com>
2025-06-25 20:06:26 -0300

    feat(modules/animals/pregnancy-diagnoses/presentation): add get one query hook

---
+++ src/app/modules/animals/presentation/hooks/queries/animal-pregnancy-diagnosis-query.hook.ts
@@ -0,0 +1,50 @@
+import { useEffect } from 'react'
+
+import { useQuery } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+import { makeRemoteGetAnimalPregnancyDiagnosisUseCase } from '../../../main/factories/use-cases/animal-pregnancy-diagnoses-use-cases'
+
+type Props = {
+  id: string
+  propertyId: string
+  animalId: string
+}
+
+export function useAnimalPregnancyDiagnosisQuery({
+  id,
+  propertyId,
+  animalId,
+}: Props) {
+  const getAnimalPregnancyDiagnosisUseCase =
+    makeRemoteGetAnimalPregnancyDiagnosisUseCase()
+
+  const {
+    data: animalPregnancyDiagnosis,
+    isError,
+    error,
+    isLoading,
+    refetch: refetchAnimalPregnancyDiagnosis,
+  } = useQuery({
+    queryKey: ['animal-pregnancy-diagnosis', id],
+    queryFn: () =>
+      getAnimalPregnancyDiagnosisUseCase.execute({
+        propertyId,
+        animalId,
+        id,
+      }),
+  })
+
+  useEffect(() => {
+    if (isError)
+      toast.error(
+        error?.message ?? 'Erro ao buscar diagnóstico de gestação do animal'
+      )
+  }, [error?.message, isError])
+
+  return {
+    animalPregnancyDiagnosis,
+    isLoading,
+    refetchAnimalPregnancyDiagnosis,
+  }
+}
