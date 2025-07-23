29824a6b5ced97cfaf3159d2cbdf2adf3f985556
Guilherme Minozzi <minozzi60@gmail.com>
2025-07-22 21:42:09 -0300

    fix: remove partial type on filters animals modules and submodules types

--- src/app/modules/animals/presentation/types/animal-childbirth-types.ts
+++ src/app/modules/animals/presentation/types/animal-childbirth-types.ts
@@ -1,5 +1,5 @@
 import type { AnimalChildbirthModel } from '../../domain/models/animal-childbirths-model'
 import type { Filters, Sort } from '@/core/domain/types'
 
-export type AnimalChildbirthFilters = Partial<Filters<AnimalChildbirthModel>>
+export type AnimalChildbirthFilters = Filters<AnimalChildbirthModel>
 export type AnimalChildbirthSort = Sort<AnimalChildbirthModel>
