c5ec0d9768366c280dd38f57a51602fd1437567c
Guilherme Minozzi <minozzi60@gmail.com>
2025-07-23 22:03:08 -0300

    fix: type error on replace animal pregnancy diagnoses

--- src/app/modules/animals/data/use-cases/animal-pregnancy-diagnoses-use-cases/remote-get-animal-pregnancy-diagnoses-use-case.ts
+++ src/app/modules/animals/data/use-cases/animal-pregnancy-diagnoses-use-cases/remote-get-animal-pregnancy-diagnoses-use-case.ts
@@ -41,8 +41,8 @@ export class RemoteGetAnimalPregnancyDiagnosesUseCase
     }
 
     const url = this.url
-      .replace(':propertyId', propertyId)
-      .replace(':animalId', animalId)
+      .replace(':propertyId', String(propertyId))
+      .replace(':animalId', String(animalId))
 
     const { statusCode, body } = await this.httpClient.request({
       url: `${url}/search`,
