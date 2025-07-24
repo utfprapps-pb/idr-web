1523f6aa7f2146453b1e2c127d623d804a3695c6
Guilherme Minozzi <minozzi60@gmail.com>
2025-07-23 21:54:28 -0300

    fix: type error on replace animal childbirths

--- src/app/modules/animals/data/use-cases/animal-childbirths-use-cases/remote-get-animal-childbirths-use-case.ts
+++ src/app/modules/animals/data/use-cases/animal-childbirths-use-cases/remote-get-animal-childbirths-use-case.ts
@@ -46,8 +46,8 @@ export class RemoteGetAnimalChildbirthsUseCase
     }
 
     const url = this.url
-      .replace(':propertyId', propertyId)
-      .replace(':animalId', animalId)
+      .replace(':propertyId', String(propertyId))
+      .replace(':animalId', String(animalId))
 
     const { statusCode, body } = await this.httpClient.request({
       url: `${url}/search`,
