edf66d94315ab0c328ce0c2bfa0eda9455fa97a8
Guilherme Minozzi <minozzi60@gmail.com>
2025-09-04 15:13:59 -0300

    feat(animals/presentation): add animal death queries hook

---
+++ src/app/modules/animals/presentation/hooks/queries/animal-death-query.hook.ts
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+
+import { useQuery } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+import { makeRemoteGetAnimalDeathUseCase } from '../../../main/factories/use-cases/animal-deaths-use-cases'
+
+type Props = {
+  id: number
+  propertyId: number
+  animalId: number
+}
+
+export function useAnimalDeathQuery({ id, propertyId, animalId }: Props) {
+  const getAnimalDeathUseCase = makeRemoteGetAnimalDeathUseCase()
+
+  const {
+    data: animalDeath,
+    isError,
+    error,
+    isLoading,
+    refetch: refetchAnimalDeath,
+  } = useQuery({
+    queryKey: ['animal-death', id],
+    queryFn: () =>
+      getAnimalDeathUseCase.execute({
+        propertyId,
+        animalId,
+        id,
+      }),
+  })
+
+  useEffect(() => {
+    if (isError) toast.error(error?.message ?? 'Erro ao buscar óbito do animal')
+  }, [error, isError])
+
+  return {
+    animalDeath,
+    isLoading,
+    refetchAnimalDeath,
+  }
+}
