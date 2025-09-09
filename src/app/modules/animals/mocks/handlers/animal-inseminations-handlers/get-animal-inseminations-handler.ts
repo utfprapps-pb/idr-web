7f2ce82f556cb2c325744207453ce8594709318a
Guilherme Minozzi <minozzi60@gmail.com>
2025-09-09 20:36:48 -0300

    feat(animals/mocks): add fake endpoints to return fake animal inseminations data

---
+++ src/app/modules/animals/mocks/handlers/animal-inseminations-handlers/get-animal-inseminations-handler.ts
@@ -0,0 +1,73 @@
+import { HttpResponse, type PathParams } from 'msw'
+
+import { HttpStatusCode } from '@/core/data/protocols/http'
+import { httpWithMiddleware } from '@/core/mocks/lib'
+import { withDelay, withAuth } from '@/core/mocks/middleware'
+import { filterData, sortData, paginateData } from '@/core/mocks/utils'
+
+import animalInseminationsData from '@database/animalInseminationsData.json'
+
+import type { AnimalInseminationApiResponse } from '../../../domain/models/animal-inseminations-model'
+import type { MockParams } from '@/core/mocks/types/mock-params-type'
+import type { MockResponse } from '@/core/mocks/types/mock-response-type'
+
+export const getAnimalInseminationsHandler = httpWithMiddleware<
+  PathParams<'propertyId' | 'animalId'>,
+  MockParams<AnimalInseminationApiResponse>,
+  MockResponse<AnimalInseminationApiResponse[]>
+>({
+  routePath:
+    '/api/properties/:propertyId/animals/:animalId/inseminations/search',
+  method: 'post',
+  middlewares: [withDelay(), withAuth],
+  resolver: async ({ request }) => {
+    const { filters, page, rows, sort } = await request.json()
+
+    if (!animalInseminationsData.length) {
+      return HttpResponse.json(
+        {
+          content: [],
+          numberOfElements: 0,
+          pageable: {
+            pageSize: 0,
+          },
+        },
+        {
+          status: 404,
+        }
+      )
+    }
+
+    let animalInseminations = animalInseminationsData
+
+    if (filters)
+      animalInseminations = filterData<AnimalInseminationApiResponse>(
+        filters,
+        animalInseminations
+      )
+    if (sort)
+      animalInseminations = sortData<AnimalInseminationApiResponse>(
+        sort,
+        animalInseminations
+      )
+
+    const numberOfElements = animalInseminations.length
+
+    if (page)
+      animalInseminations = paginateData<AnimalInseminationApiResponse>(
+        { page, perPage: rows },
+        animalInseminations
+      )
+
+    return HttpResponse.json(
+      {
+        content: animalInseminations,
+        numberOfElements,
+        pageable: {
+          pageSize: rows,
+        },
+      },
+      { status: HttpStatusCode.ok }
+    )
+  },
+})
