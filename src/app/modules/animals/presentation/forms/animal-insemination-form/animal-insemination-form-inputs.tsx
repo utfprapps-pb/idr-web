2c998ea2a95868739e1c7f4dfdcd802be5bef111
Guilherme Minozzi <minozzi60@gmail.com>
2025-09-17 20:49:00 -0300

    feat(modules/animals/presentation): add animal insemination form

---
+++ src/app/modules/animals/presentation/forms/animal-insemination-form/animal-insemination-form-inputs.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react'
+
+import { useFormContext } from 'react-hook-form'
+
+import { DatePicker, Form, Combobox } from '@/core/presentation/components/ui'
+import { useDebounce } from '@/core/presentation/hooks'
+
+import { useAnimalInseminationContext } from '../../hooks/animal-insemination-context.hook'
+import { useAllAnimalsQuery } from '../../hooks/queries/all-animals-query.hook'
+import { AnimalInseminationFormSchema } from '../../validations/animal-insemination-form-schema'
+
+export function AnimalInseminationFormInputs() {
+  const form = useFormContext<AnimalInseminationFormSchema>()
+  const { propertyId } = useAnimalInseminationContext()
+
+  const [searchAnimal, setSearchAnimal] = useState('')
+
+  const debouncedAnimal = useDebounce({ value: searchAnimal })
+
+  const { allAnimals, isLoading } = useAllAnimalsQuery({
+    propertyId,
+    filters: {
+      name: {
+        value: debouncedAnimal,
+        type: 'LIKE',
+      },
+    },
+  })
+
+  return (
+    <>
+      <Form.Field
+        name="date"
+        control={form.control}
+        render={({ field, fieldState }) => {
+          const { error } = fieldState
+
+          return (
+            <Form.Item>
+              <Form.Label>Data da Inseminação Artificial*</Form.Label>
+              <Form.Control>
+                <DatePicker
+                  date={field.value}
+                  onSelect={field.onChange}
+                  isError={!!error?.message}
+                />
+              </Form.Control>
+              <Form.Message />
+            </Form.Item>
+          )
+        }}
+      />
+
+      <Form.Field
+        name="sire"
+        control={form.control}
+        render={({ field, fieldState }) => {
+          const { error } = fieldState
+
+          return (
+            <Form.Item>
+              <Form.Label>Identificação do Animal Reprodutor*</Form.Label>
+              <Form.Control>
+                <Combobox
+                  search={searchAnimal}
+                  items={allAnimals}
+                  loading={isLoading}
+                  selected={field.value}
+                  handleSearch={setSearchAnimal}
+                  handleSelect={field.onChange}
+                  isError={!!error}
+                  placeholder="Selecione o animal reprodutor"
+                  emptyMessage="Nenhuma animal encontrado"
+                  searchPlaceholder="Buscar animal reprodutor"
+                />
+              </Form.Control>
+              <Form.Message />
+            </Form.Item>
+          )
+        }}
+      />
+    </>
+  )
+}
+
+AnimalInseminationFormInputs.displayName = 'AnimalInseminationFormInputs'
