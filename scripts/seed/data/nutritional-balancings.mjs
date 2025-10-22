4da137f304e3cb9d8a71b1a8cd9ede84d2a87d66
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-22 10:25:39 -0300

    feat(scripts): add scripts to generate fake data

---
+++ scripts/seed/data/nutritional-balancings.mjs
@@ -0,0 +1,26 @@
+import { faker } from '@faker-js/faker/locale/pt_BR'
+
+export const nutritionalBalancingsData = Array.from(
+  { length: faker.number.int({ min: 1, max: 100 }) },
+  (_, index) => ({
+    id: index + 1,
+    date: faker.date.past().toISOString(),
+    animal: faker.lorem.word(),
+    breed: faker.animal.cow(),
+    weight: faker.number
+      .float({ min: 200, max: 700, precision: 0.1 })
+      .toFixed(2),
+    milkProduction: faker.number
+      .float({
+        min: 5,
+        max: 50,
+      })
+      .toFixed(2),
+    estimatedMilkProduction: faker.number
+      .float({
+        min: 5,
+        max: 50,
+      })
+      .toFixed(2),
+  })
+)
