2fed4e446144581e158075146379859362832839
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-29 20:08:30 -0300

    fix: badge secondary variant

--- src/core/presentation/components/ui/badge/badge.tsx
+++ src/core/presentation/components/ui/badge/badge.tsx
@@ -13,7 +13,7 @@ const badgeVariants = cva(
         default:
           'border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90',
         secondary:
-          'border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90',
+          'border-transparent bg-slate-200 text-secondary-foreground [a&]:hover:bg-slate-200/90',
         destructive:
           'border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
