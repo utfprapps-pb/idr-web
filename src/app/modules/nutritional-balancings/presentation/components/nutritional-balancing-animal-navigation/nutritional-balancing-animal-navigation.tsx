309d0c5da589f67f6c2349e5412f23d483866a2c
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-29 21:13:07 -0300

    feat(nutritional-balancings/presentation): add nutritional balancing animal navigation component

---
+++ src/app/modules/nutritional-balancings/presentation/components/nutritional-balancing-animal-navigation/nutritional-balancing-animal-navigation.tsx
@@ -0,0 +1,72 @@
+import { ChevronLeftIcon, ChevronRightIcon } from 'lucide-react'
+
+import { Button, Card } from '@/core/presentation/components/ui'
+import { Badge } from '@/core/presentation/components/ui/badge'
+
+type NutritionalBalancingAnimalNavigationProps = {
+  currentNutritionalBalancingIndex: number
+  totalNutritionalBalancings: number
+  animalName: string
+  handleSelectNutritionalBalancing: (index: number) => void
+}
+
+export function NutritionalBalancingAnimalNavigation({
+  currentNutritionalBalancingIndex,
+  totalNutritionalBalancings,
+  animalName,
+  handleSelectNutritionalBalancing,
+}: Readonly<NutritionalBalancingAnimalNavigationProps>) {
+  const hasPrevious =
+    currentNutritionalBalancingIndex !== null &&
+    currentNutritionalBalancingIndex > 0
+  const hasNext =
+    currentNutritionalBalancingIndex !== null &&
+    currentNutritionalBalancingIndex < totalNutritionalBalancings - 1
+
+  return (
+    <Card.Container>
+      <Card.Content className="flex justify-between items-center">
+        <Button
+          type="button"
+          variant="outline"
+          disabled={!hasPrevious}
+          onClick={() =>
+            handleSelectNutritionalBalancing(
+              currentNutritionalBalancingIndex - 1
+            )
+          }
+        >
+          <ChevronLeftIcon className="mr-2" />
+          Anterior
+        </Button>
+
+        <div className="flex flex-col items-center gap-2">
+          <h2 className="text-2xl font-semibold text-primary-500">
+            {animalName}
+          </h2>
+
+          <Badge variant="secondary">
+            {`Animal ${currentNutritionalBalancingIndex + 1} de ${totalNutritionalBalancings}`}
+          </Badge>
+        </div>
+
+        <Button
+          type="button"
+          variant="outline"
+          disabled={!hasNext}
+          onClick={() =>
+            handleSelectNutritionalBalancing(
+              currentNutritionalBalancingIndex + 1
+            )
+          }
+        >
+          Próximo
+          <ChevronRightIcon className="ml-2" />
+        </Button>
+      </Card.Content>
+    </Card.Container>
+  )
+}
+
+NutritionalBalancingAnimalNavigation.displayName =
+  'NutritionalBalancingAnimalNavigation'
