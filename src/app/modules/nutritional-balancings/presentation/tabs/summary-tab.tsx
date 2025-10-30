ba99cd7c716ae992138f4864325ee0ed9f244baa
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-30 19:24:58 -0300

    feat(nutritional-balancings/presentation): add summary tab

---
+++ src/app/modules/nutritional-balancings/presentation/tabs/summary-tab.tsx
@@ -0,0 +1,22 @@
+import { NutritionalBalancingAnimalInformation } from '../components/nutritional-balancing-animal-information'
+import { NutritionalBalancingSummary } from '../components/nutritional-balancing-summary'
+
+import type { Option } from '@/core/domain/types'
+
+type SummaryTabProps = {
+  animalInformationItems: Option<string>[]
+  nutritionalSummaryItems: Option<string>[]
+}
+
+export function SummaryTab({
+  animalInformationItems,
+  nutritionalSummaryItems,
+}: Readonly<SummaryTabProps>) {
+  return (
+    <section className="space-y-4">
+      <NutritionalBalancingAnimalInformation items={animalInformationItems} />
+
+      <NutritionalBalancingSummary items={nutritionalSummaryItems} />
+    </section>
+  )
+}
