5312d06c4e6854ae7b91226272353c20a05d9723
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-30 19:25:31 -0300

    feat(nutritional-balancings/presentation): add util function to create empty nutritional balancing

---
+++ src/app/modules/nutritional-balancings/presentation/utils/create-empty-nutritional-balancing-entry.ts
@@ -0,0 +1,60 @@
+import type { NutritionalBalancingSchema } from '../validations/nutritional-balancing-form-schema'
+
+export function createEmptyNutritionalBalancingEntry(
+  nutritionalBalancing: Partial<NutritionalBalancingSchema>
+) {
+  return {
+    ...nutritionalBalancing,
+    animal: {
+      id: 0,
+      name: '',
+      breed: '',
+      ecc: '',
+      weight: '',
+      milkProduction: '',
+      estimatedMilkProduction: '',
+      ...nutritionalBalancing.animal,
+    },
+    summary: {
+      totalDryMatter: '10',
+      etherExtractPercent: '20',
+      forageDryMatterPercent: '30',
+      concentrateDryMatterPercent: '40',
+      nonFibrousCarbohydratesPercent: '50',
+      rdpTdnRatio: '60',
+    },
+    evaluations: [
+      {
+        nutrientName: 'IMS (Ingestão de Matéria Seca)',
+        providedValue: '10',
+        requiredValue: '15',
+        evaluationStatus: 'BELOW' as const,
+      },
+      {
+        nutrientName: 'NDT (Nutrientes Digestíveis Totais)',
+        providedValue: '15',
+        requiredValue: '15',
+        evaluationStatus: 'NORMAL' as const,
+      },
+      {
+        nutrientName: 'PB (Proteína Bruta)',
+        providedValue: '15',
+        requiredValue: '10',
+        evaluationStatus: 'ABOVE' as const,
+      },
+      {
+        nutrientName: 'Ca (Cálcio)',
+        providedValue: '15',
+        requiredValue: '10',
+        evaluationStatus: 'ABOVE' as const,
+      },
+      {
+        nutrientName: 'P (Fósforo)',
+        providedValue: '15',
+        requiredValue: '17',
+        evaluationStatus: 'NORMAL' as const,
+      },
+    ],
+    ingredientGroups: [],
+  }
+}
