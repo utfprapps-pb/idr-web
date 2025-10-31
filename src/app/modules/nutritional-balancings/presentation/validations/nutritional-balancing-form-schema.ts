cb27c0b460aede74d2fce43100f8197c2bb2b18d
Guilherme Minozzi <minozzi60@gmail.com>
2025-10-31 19:28:50 -0300

    feat(nutritional-balancings/presentation): improvement nutritional balancing form validations

--- src/app/modules/nutritional-balancings/presentation/validations/nutritional-balancing-form-schema.ts
+++ src/app/modules/nutritional-balancings/presentation/validations/nutritional-balancing-form-schema.ts
@@ -1,13 +1,9 @@
 import { array, z } from 'zod'
 
-import { optionSchema } from '@/core/validation/schemas'
-
 const nutritionalBalancingAnimalSchema = z.object({
   id: z.number().min(1, { message: 'Animal é obrigatório' }),
   name: z.string().min(1, { message: 'Nome do animal é obrigatório' }),
-  breed: optionSchema.refine(({ label, value }) => label !== '' && value > 0, {
-    message: 'Raça é obrigatória',
-  }),
+  breed: z.string().min(1, { message: 'Raça é obrigatória' }),
   ecc: z.string().min(1, { message: 'ECC é obrigatório' }),
   weight: z.string().min(1, { message: 'Peso é obrigatório' }),
   milkProduction: z
@@ -68,20 +64,42 @@ const ingredientGroupSchema = z.object({
   }),
 })
 
+const nutritionalBalancingSchema = z
+  .object({
+    animal: nutritionalBalancingAnimalSchema,
+    summary: nutritionalBalancingSummarySchema,
+    evaluations: z
+      .array(nutritionalEvaluationSchema)
+      .min(1, { message: 'Adicione ao menos uma avaliação' }),
+    ingredientGroups: z
+      .array(ingredientGroupSchema)
+      .min(1, { message: 'Adicione ao menos um grupo de ingredientes' }),
+  })
+  .superRefine((value, context) => {
+    if (!value?.animal || !value.animal.id || value.animal.id < 1) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Selecione o animal',
+        path: ['animal'],
+      })
+    }
+  })
+
+const nutritionalBalancingsSchema = z.array(nutritionalBalancingSchema).min(1, {
+  message: 'Adicione ao menos um balanceamento nutricional para um animal',
+})
+
 export const nutritionalBalancingFormSchema = z.object({
   date: z
     .date()
     .max(new Date(), { message: 'A data deve ser menor que a data atual' }),
-  animal: nutritionalBalancingAnimalSchema,
-  summary: nutritionalBalancingSummarySchema,
-  evaluations: z
-    .array(nutritionalEvaluationSchema)
-    .min(1, { message: 'Adicione ao menos uma avaliação' }),
-  ingredientGroups: z
-    .array(ingredientGroupSchema)
-    .min(1, { message: 'Adicione ao menos um grupo de ingredientes' }),
+  nutritionalBalancings: nutritionalBalancingsSchema,
 })
 
 export type NutritionalBalancingFormSchema = z.infer<
   typeof nutritionalBalancingFormSchema
+>
+
+export type NutritionalBalancingSchema = z.infer<
+  typeof nutritionalBalancingSchema
 >
