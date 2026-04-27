dd74efdcb67a300b3465dbd5c21a1bad0527b822
Guilherme Minozzi <minozzi60@gmail.com>
2026-04-27 14:04:29 -0300

    fix: get all animals with list endpoint

--- src/app/modules/animals/domain/use-cases/get-all-animals-use-case.ts
+++
@@ -1,7 +0,0 @@
-import type { AnimalModel } from '../models/animals-model'
-import type { RequestInterface } from '@/core/domain/types'
-
-export type GetAllAnimalsUseCase = RequestInterface<
-  { propertyId: number },
-  AnimalModel[]
->
