c8de3b55031eab24c6bb05901d86e12c94ef6ead
MatheusDellaVechia <matheusdellavechia00@gmail.com>
2026-05-03 16:56:45 -0300

    feat: change HTTP method from patch to put for general cultivation update

--- src/app/modules/general-cultivations/data/use-cases/general-cultivations-use-cases/remote-update-general-cultivation-use-case.ts
+++ src/app/modules/general-cultivations/data/use-cases/general-cultivations-use-cases/remote-update-general-cultivation-use-case.ts
@@ -20,11 +20,15 @@ export class RemoteUpdateGeneralCultivationUseCase
   }) => {
     const { statusCode } = await this.httpClient.request({
       url: `${this.url}/${id}`,
-      method: 'patch',
+      method: 'put',
       body: generalCultivation,
     })
 
-    if (statusCode === HttpStatusCode.noContent) return
+    if (
+      statusCode === HttpStatusCode.noContent ||
+      statusCode === HttpStatusCode.ok
+    )
+      return
 
     if (statusCode === HttpStatusCode.badRequest) throw new BadRequestError()
 
